dcd2537e014411f43bf6bf95c68166cd55255b71
nil-sj <niladri123@gmail.com>
2020-08-27 20:14:41 -0400

    Week 5 - Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem,
         Button, Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 
 const maxLength = len => val => !val || (val.length <= len);
@@ -138,7 +139,27 @@ class CommentForm extends Component {
 
 
 function CampsiteInfo(props) {
-    if(props.campsite) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    if (props.campsite) {
         return (
             <div className="container">
                 <div className="row">
